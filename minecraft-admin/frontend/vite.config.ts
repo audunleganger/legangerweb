5bae119c3a5006e1c1fdb5d2e9cc7c4fa65a01cb
Audun Leganger <audun.leganger@gmail.com>
2025-05-20 22:03:29 +0000

    Clean up container structure

--- minecraft-admin/frontend/vite.config.ts
+++ minecraft-admin/frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     plugins: [react()],
     server: {
         host: "0.0.0.0",
-        port: 5000,
+        port: 3000,
         proxy: {
             "/api": "http://backend:8000",
         },
